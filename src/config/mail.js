99b19e461cbf22b035d5fe47fd838076bb219e71
Igor Melo <igorjmelo4@gmail.com>
2019-10-30 23:07:14 -0300

    adding mailer and cancelling email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3d18204227c02a',
+    pass: '63d7b45416b44a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
