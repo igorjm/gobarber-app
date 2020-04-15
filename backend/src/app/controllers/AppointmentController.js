7c480a14491a2223cff2d46b96257ee0ff022fce
igor.melo <igorjmelo4@gmail.com>
2020-04-15 20:59:05 -0300

    listing appointments and provider schedule

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,11 +1,40 @@
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import * as Yup from 'yup';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.user_id, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
