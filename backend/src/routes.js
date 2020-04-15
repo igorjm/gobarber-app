71189589c009d089dc308d681f7b477411856761
igor.melo <igorjmelo4@gmail.com>
2020-04-14 23:17:38 -0300

    appointments and providers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,11 +16,15 @@ const upload = multer(multerConfig);
 //STORE
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(authMiddleware);
 
 //UPDATE
 routes.put('/users', UserController.update);
+
+//LIST
+routes.get('/providers', ProviderController.index);
 
 export default routes;
