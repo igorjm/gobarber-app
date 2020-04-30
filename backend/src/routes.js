d9ff0608b11b08aa5891f187e399fe082c28a3d7
igor.melo <igorjmelo4@gmail.com>
2020-04-29 21:57:19 -0300

    ADD: Checking provider availability

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -29,6 +30,7 @@ routes.put('/notifications/:id', NotificationController.update);
 
 //LIST
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
